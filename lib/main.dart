7a5e63a3170e507e85ebeaf399b81777b0c24eff
Joel Andrade <joelmanrique38@gmail.com>
2025-09-02 15:41:40 -0600

    se añadieron email y contraseña

--- lib/main.dart
+++ lib/main.dart
@@ -21,3 +21,16 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// This widget is the root of your application.
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    ),
+    home: const LoginScreen(),
+  );
+}
